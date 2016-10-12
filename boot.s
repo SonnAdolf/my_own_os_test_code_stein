82ac35a03e6638c5e9949c62a285811b6586bd45
SonnAdolf <programeryu@163.com>
2016-10-12 21:55:22 +0800

    change code for at&t assembly.

--- boot.s
+++ boot.s
@@ -11,7 +11,7 @@ _start:
 
 	movw $str, %ax
 	movw %ax, %bp
-	movw $16, %cx
+	movw $0x10, %cx
 	movw $0x1301, %ax
 	movw $0x000c, %bx
 	movb $0, %dl
@@ -20,4 +20,4 @@ loop:
 	jmp loop
 str:	.ascii "my stein--my os."
 	.org 510
-	.word 0xaa55
+	.word 0xAA55
